4905b2ee848c26c23a01233fc2d94386a65b8aa4
Yuji Yamamoto <y.yama683@gmail.com>
2020-11-07 13:20:11 +0900

    fix hardcode

--- app/config/settings.py
+++ app/config/settings.py
@@ -75,7 +75,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('172.21.0.1', 6379)],
+            "hosts": [('localhost', 6379)],
         },
     },
 }
